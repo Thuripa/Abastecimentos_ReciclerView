ccf02f5a7c3a39c56880a8b3d78de7e473999a52
Guilherme <guilhermemelo@protonmail.com>
2019-10-16 21:43:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/abastecimentosreciclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.abastecimentosreciclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
